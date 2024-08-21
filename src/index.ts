f2d23ba18b3466a26a45bde7bf44591f040c7b20
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2024-08-22 01:05:59 +0530

    environment setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, TypeScript with Node.js!");
+import express, {  Request, Response } from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Ascend backend server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
